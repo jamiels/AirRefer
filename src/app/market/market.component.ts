e6936ac164507e186f11f17f698e99c73421fdac
Jamiel Sheikh <i@jamiel.net>
2018-08-06 03:45:36 -0400

    early commits

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./market.component.css"]
 })
 export class MarketComponent implements OnInit {
-  rows = [{ id: "Austin", entity: "Male", category: "Swimlane" }];
+  rows = [
+    { id: "Austin", entity: "Male", category: "Swimlane" },
+    { id: "Austin", entity: "Male", category: "DDD" },
+    { id: "Austin", entity: "Male", category: "XX" },
+    { id: "Austin", entity: "Male", category: "ZZ" },
+    { id: "ddd", entity: "Male", category: "XX" },
+    { id: "xxx", entity: "Male", category: "XX" },
+    { id: "qq", entity: "Male", category: "XX" },
+    { id: "rr", entity: "Male", category: "XX" },
+    { id: "tt", entity: "Male", category: "XX" },
+    { id: "nnn", entity: "Male", category: "XX" },
+    { id: "uuu", entity: "Male", category: "XX" },
+    { id: "ttt", entity: "Male", category: "XX" },
+    { id: "eee", entity: "Male", category: "XX" },
+    { id: "ooo", entity: "Male", category: "XX" },
+    { id: "qqq", entity: "Male", category: "XX" },
+    { id: "yy", entity: "Male", category: "XX" },
+    { id: "nnn", entity: "Male", category: "XX" }
+  ];
 
   columns = [
     { name: "ID" },
@@ -22,6 +40,10 @@ export class MarketComponent implements OnInit {
     { name: "Interest/Bids" },
     { name: "Action" }
   ];
+
+  scrollBarHorizontal = window.innerWidth < 960;
+  columnModeSetting = window.innerWidth < 960 ? "standard" : "force";
+
   //columns = [{ prop: "name" }, { name: "Gender" }, { name: "Company" }];
 
   constructor() {}
