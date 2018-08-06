1019904dd750d95c755390bb97a28ca611bbbf97
Jamiel Sheikh <i@jamiel.net>
2018-08-06 02:19:05 -0400

    early commits

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -6,12 +6,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./market.component.scss"]
 })
 export class MarketComponent implements OnInit {
-  rows = [
-    { name: "Austin", gender: "Male", company: "Swimlane" },
-    { name: "Dany", gender: "Male", company: "KFC" },
-    { name: "Molly", gender: "Female", company: "Burger King" }
+  rows = [{ id: "Austin", entity: "Male", category: "Swimlane" }];
+
+  columns = [
+    { name: "ID" },
+    { name: "Entity" },
+    { name: "Category" },
+    { name: "Type" },
+    { name: "Referrer" },
+    { name: "Jurisdiction" },
+    { name: "Region" },
+    { name: "Potential Deal Size" },
+    { name: "Potential GCI" },
+    { name: "Award Method" },
+    { name: "Interest/Bids" },
+    { name: "Action" }
   ];
-  columns = [{ prop: "name" }, { name: "Gender" }, { name: "Company" }];
+  //columns = [{ prop: "name" }, { name: "Gender" }, { name: "Company" }];
 
   constructor() {}
 
